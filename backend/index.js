3f1e4022ddfdafdd2293736897c36a51e827ed28
yanghd <yanghd@vt.edu>
2023-12-11 02:49:22 -0500

    Set up the connection to MongoDB database

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config(); // Load .env file content into process.env
+
+// Connect to MongoDB database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(console.log("Connected to mongoDB server successfully"))
+  .catch((error) => {
+    console.log(error);
+  });
 
 const port = process.env.PORT;
 
 const app = express();
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
-
+  console.log(`Server is running on port ${port}`);
+});
