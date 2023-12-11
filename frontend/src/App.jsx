daf0c58f3c3a7b40984ebc40b3fd6d5c0fa72ab2
yanghd <yanghd@vt.edu>
2023-12-10 21:01:18 -0500

    Set up the routes using react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
+import {Outlet} from "react-router-dom";
 
 function App() {
   return (
     <>
-    <h1 className="text-orange-400">TESTING...</h1>
+      <Outlet/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App.jsx";
+import Home from "./screens/Home.jsx";
+import About from "./screens/About.jsx";
+import Login from "./screens/Login.jsx";
+import Register from "./screens/Register.jsx";
+import Profile from "./screens/Profile.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}>
+
+    {/* Default child route */}
+    <Route index={true} path="/" element={<Home />}/> 
+    <Route path="/about" element={<About />}/> 
+    <Route path="/login" element={<Login />}/> 
+    <Route path="/register" element={<Register />}/> 
+    <Route path="/profile" element={<Profile />}/> 
+  </Route>)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
