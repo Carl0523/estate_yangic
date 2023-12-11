59efbb1a03dd53277ce9770af69e9ab57b018691
yanghd <yanghd@vt.edu>
2023-12-11 18:05:52 -0500

    Create a file to store custom error and set up a middleware error function

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
@@ -30,3 +32,14 @@ app.listen(port, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const errorMessage = err.message || "Internal Server error";
+    res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message: errorMessage
+    })
+    next();
+}) 
