e6bba3d51657eb685cfbb8b7248efd503be87ba3
yanghd <yanghd@vt.edu>
2023-12-14 22:44:56 -0500

    Add some custom border radius

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors : {
+      colors: {
         primary: "rgb(82, 89, 212)",
         alertFont: "rgb(193, 113, 112)",
         alertContainer: "rgb(244, 237, 236)",
       },
       screens: {
-        xs : "400px",
+        xs: "400px",
       },
       flex: {
-        '2': '2 2 0%',
-        '3': '3 3 0%', 
+        2: "2 2 0%",
+        3: "3 3 0%",
       },
       boxShadow: {
-        leftShadow: "-2px 0 5px 5px gray"
+        leftShadow: "-2px 0 5px 5px gray",
+      },
+      borderRadius: {
+        inputRadius: "0.125rem",
+        buttonRadius: "0.375rem"
       }
     },
   },
   plugins: [],
-}
+};
