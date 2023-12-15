db3564cf94d0dca15d2f9e95d223ac1c30dd5c17
yanghd <yanghd@vt.edu>
2023-12-15 16:20:30 -0500

    Finished updateUser route and add a verifyToken middleware for update api route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,11 +1,39 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
 
+/**
+ * 
+ * @param req The request from the client side and verifyToken function
+ * @param res The response sending back to the client side
+ * @param next mainly used for calling the middleware error handler
+ */
+const updateUser = async (req, res, next) => {
+    if (req.userId != req.params.id) return next(errorHandler(401, "UnAuthenticated"));
 
+    try {
+        if (req.body.password)
+        {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
 
-const test = (req, res) => {
-    res.json({
-        message: "Test"
-    })
+        {/* $set will make sure the user params only update if they're present */}
+        const updatedUser = await User.findByIdAndUpdate(req.userId, {
+            $set:{
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
+            }
+        }, {new: true})
+
+        const {password, ...rest} = updatedUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
+
 }
 
 
-export {test};
+export {updateUser};
---
+++ backend/utils/verifyToken.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import {errorHandler} from './error.js';
+import User from '../models/user.model.js';
+
+/**
+ * Verify if user is authenticate by checking the token
+ * @param req The request
+ * @param res The response
+ * @param next function that go the the next step
+ */
+export const verifyToken = (req, res, next) => {
+    {/* 1. Get the token from cookie */}
+    const token = req.cookies.access_token;
+
+    {/* 2. Check if token exist */}
+    if (!token) return next(errorHandler(401, "Unauthorized"));
+
+    {/* 3. Verify the token:
+        success: store the userId to the req.user
+        fail: Return the corresponding error
+    */}
+    jwt.verify(token, process.env.JWT_KEY, (error, payload) => {
+        if (error) return next(errorHandler(403, 'Something went wrong'));
+        req.userId = payload.id;
+        next();
+    })
+
+}
