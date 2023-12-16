99f2a6320ab8b5691a310d2411b26b98d42f74c2
yanghd <yanghd@vt.edu>
2023-12-16 01:35:06 -0500

    Add customCss prop: Allowed flexible style

--- frontend/src/components/ErrorMessage.jsx
+++ frontend/src/components/ErrorMessage.jsx
@@ -1,11 +1,10 @@
 import { GrCircleAlert } from "react-icons/gr";
 
-const alertContainer =
-  "flex flex-col gap-2 items-center lg:w-4/6 w-5/6 p-3 text-center bg-alertContainer m-2 rounded-md shadow-xl sm:text-base text-sm";
-
-export default function ErrorMessage({ errorMessage }) {
+export default function ErrorMessage({ errorMessage, customCss = "" }) {
   return (
-    <div className={alertContainer}>
+    <div
+      className={`flex flex-col gap-2 items-center lg:w-4/6 w-5/6 p-3 text-center bg-alertContainer m-2 rounded-md shadow-xl sm:text-base text-sm ${customCss}`}
+    >
       <GrCircleAlert className="text-alertFont sm:text-2xl text-xl" />
       <p className="text-alertFont">{errorMessage}</p>
     </div>
