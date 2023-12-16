9f282d9f786afb57fe0784d8e92b6fb6000f602d
yanghd <yanghd@vt.edu>
2023-12-16 04:29:04 -0500

    Add images

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,11 +1,19 @@
-import logo from './logo2.svg';
-import blackLogo from './logo2.svg';
-import registerHouse from './register_house.jpeg';
-import registerMansion from './register_mansion.jpg'
-import registerMansion2 from './register_mansion2.jpg'
-import loginHouse from './login_house.jpg'
-import loginMansion from './login_mansion.jpg'
+import logo from "./logo2.svg";
+import blackLogo from "./logo2.svg";
+import registerHouse from "./register_house.jpeg";
+import registerMansion from "./register_mansion.jpg";
+import registerMansion2 from "./register_mansion2.jpg";
+import loginHouse from "./login_house.jpg";
+import loginMansion from "./login_mansion.jpg";
+import trashCan from "./trashcan.png";
 
-
-
-export {logo, blackLogo, registerHouse, registerMansion, registerMansion2, loginHouse, loginMansion};
+export {
+  logo,
+  blackLogo,
+  registerHouse,
+  registerMansion,
+  registerMansion2,
+  loginHouse,
+  loginMansion,
+  trashCan,
+};
