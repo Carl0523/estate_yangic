774a51e74dfddd21ffb0146b96b3e8109ae5ac14
yanghd <yanghd@vt.edu>
2023-12-16 01:38:59 -0500

    Add some error handling for duplicate email

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,37 +3,49 @@ import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 
 /**
- * 
+ *
  * @param req The request from the client side and verifyToken function
  * @param res The response sending back to the client side
  * @param next mainly used for calling the middleware error handler
  */
 const updateUser = async (req, res, next) => {
-    if (req.userId != req.params.id) return next(errorHandler(401, "UnAuthenticated"));
+  if (req.userId != req.params.id)
+    return next(errorHandler(401, "UnAuthenticated"));
 
-    try {
-        if (req.body.password)
-        {
-            req.body.password = bcryptjs.hashSync(req.body.password, 10);
-        }
+    
+  if (req.body.email) {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return next(errorHandler(500, "The email has already been taken"));
+    }
+  }
 
-        {/* $set will make sure the user params only update if they're present */}
-        const updatedUser = await User.findByIdAndUpdate(req.userId, {
-            $set:{
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: req.body.avatar
-            }
-        }, {new: true})
-
-        const {password, ...rest} = updatedUser._doc;
-        res.status(200).json(rest);
-    } catch (error) {
-        next(error)
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
 
-}
+    {
+      /* $set will make sure the user params only update if they're present */
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
 
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export {updateUser};
+export { updateUser };
