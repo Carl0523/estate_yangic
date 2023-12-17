86368817c8a89d8e2a2029c4e4a2d369c985a168
yanghd <yanghd@vt.edu>
2023-12-17 18:01:51 -0500

    Add a custom button component

---
+++ frontend/src/components/CustomButton.jsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion";
+
+export default function CustomButton({
+  type = "button",
+  text,
+  icon = null,
+  bgColor = "black",
+  textColor = "white",
+  responsiveWidth = "",
+  buttonHandler = null,
+  margin = "mt-3",
+  padding = "p-3",
+}) {
+  return (
+    <motion.button
+      type={type}
+      whileHover={{ scale: 1.03, opacity: 0.9 }}
+      whileTap={{ scale: 0.95 }}
+      onClick={buttonHandler || (() => {})}
+      className={`${responsiveWidth} bg-${bgColor} text-${textColor} ${margin} ${padding} rounded-buttonRadius`}
+    >
+      {text}
+    </motion.button>
+  );
+}
