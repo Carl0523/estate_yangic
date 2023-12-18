e988af386ba38eef059dc9488528302e865b5d31
yanghd <yanghd@vt.edu>
2023-12-17 21:56:41 -0500

    Add prop that allow CustomButton to take disabled condition

--- frontend/src/components/CustomButton.jsx
+++ frontend/src/components/CustomButton.jsx
@@ -9,14 +9,16 @@ export default function CustomButton({
   buttonHandler = null,
   margin = "mt-3",
   padding = "p-3",
+  disabledCondition = null
 }) {
   return (
     <motion.button
       type={type}
+      disabled={disabledCondition ? disabledCondition : false}
       whileHover={{ scale: 1.03, opacity: 0.9 }}
       whileTap={{ scale: 0.95 }}
       onClick={buttonHandler || (() => {})}
-      className={`${responsiveWidth} bg-${bgColor} text-${textColor} ${margin} ${padding} rounded-buttonRadius`}
+      className={`${responsiveWidth} bg-${bgColor} text-${textColor} ${margin} ${padding} rounded-buttonRadius disabled:bg-gray-300`}
     >
       {text}
     </motion.button>
--- frontend/src/screens/AddHome.jsx
+++ frontend/src/screens/AddHome.jsx
@@ -14,7 +14,17 @@ import ImageItem from "../components/ImageItem";
 export default function AddHome() {
   const [imgFiles, setImgFiles] = useState([]);
   const [isUploading, setIsUploading] = useState(false);
+  const [error, setError] = useState("");
   const [form, setForm] = useState({
+    name: "",
+    description: "",
+    address: "",
+    price: 0,
+    numOfBedrooms: 1,
+    numOfBathrooms: 1,
+    furnished: false,
+    parking: false,
+    type: "sale",
     imageUrls: [],
   });
 
@@ -85,6 +95,32 @@ export default function AddHome() {
     });
   };
 
+  /**
+   * Handle the value change for the entire form
+   * @param e the event listener object
+   */
+  const handleFormChange = (e) => {
+    // 1. Identify the rent and sale check boxes
+    if (e.target.id === "rent" || e.target.id === "sale") {
+      setForm({ ...form, type: e.target.id });
+    }
+    // 2. Identify the parking and furnished check boxes
+    else if (e.target.id === "parking" || e.target.id === "furnished") {
+      setForm({ ...form, [e.target.id]: e.target.checked });
+    } else {
+      setForm({ ...form, [e.target.id]: e.target.value });
+    }
+  };
+
+  /**
+   * Submit the form to the server side and store in the database
+   * @param {*} e 
+   */
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
     <motion.main
       initial={{ opacity: 0, translateY: "-200" }}
@@ -97,73 +133,108 @@ export default function AddHome() {
       <h1 className="mb-3 text-center text-2xl font-semibold">Add New Home</h1>
 
       {/* 2. The form */}
-      <form className="sm:w-2/3 2-full flex flex-col gap-5 justify-center items-center mx-auto">
+      <form onSubmit={handleFormSubmit} className="sm:w-2/3 2-full flex flex-col gap-5 justify-center items-center mx-auto">
         {/* 2a. The name of the home */}
         <div className="flex flex-col md:w-2/3 w-full gap-1">
-          <label htmlFor="name" className="font-semibold">
+          <label
+            htmlFor="name"
+            className="font-semibold after:content-['*'] after:text-red-500"
+          >
             Name
           </label>
           <input
             type="text"
             id="name"
-            placeholder="Shawnee Department"
+            value={form.name}
+            placeholder="Squire Center"
             minLength="4"
             maxLength="50"
             className="outline outline-gray-500 focus:outline-blue-500 p-3 rounded-inputRadius focus:outline-2"
             required
+            onChange={handleFormChange}
           />
         </div>
 
         {/* 2b. The address of the home */}
         <div className="flex flex-col md:w-2/3 w-full gap-1">
-          <label htmlFor="address" className="font-semibold">
+          <label
+            htmlFor="address"
+            className="font-semibold after:content-['*'] after:text-red-500"
+          >
             Address
           </label>
           <input
             type="text"
             id="address"
-            placeholder="Enter your home address"
+            value={form.address}
+            placeholder="290 College Ave, Blacksburg, VA, 24060"
             minLength="4"
             maxLength="50"
             className="outline p-3 outline-gray-500 focus:outline-blue-500 rounded-inputRadius focus:outline-2"
             required
+            onChange={handleFormChange}
           />
         </div>
 
         {/* 2c. The description of the home */}
         <div className="flex flex-col md:w-2/3 w-full gap-1">
-          <label htmlFor="description" className="font-semibold">
+          <label
+            htmlFor="description"
+            className="font-semibold after:content-['*'] after:text-red-500"
+          >
             Description
           </label>
           <textarea
             type="text"
             id="description"
+            value={form.description}
             placeholder="Add description"
             minLength="1"
             className="outline p-3 outline-gray-500 focus:outline-blue-500 rounded-inputRadius focus:outline-2"
             required
+            onChange={handleFormChange}
           />
         </div>
 
         {/* 2d. The checking boxes: rent, sale, parking, furnished */}
         <div className="md:w-2/3 w-full flex flex-wrap justify-center gap-5">
           <div className="flex gap-2">
-            <input type="checkbox" id="sale" />
+            <input
+              type="checkbox"
+              id="sale"
+              checked={form.type === "sale"}
+              onChange={handleFormChange}
+            />
             <label htmlFor="sale">Sale</label>
           </div>
 
           <div className="flex gap-2">
-            <input type="checkbox" id="rent" />
+            <input
+              type="checkbox"
+              id="rent"
+              checked={form.type === "rent"}
+              onChange={handleFormChange}
+            />
             <label htmlFor="rent">Rent</label>
           </div>
 
           <div className="flex gap-2">
-            <input type="checkbox" id="parking" />
+            <input
+              type="checkbox"
+              id="parking"
+              checked={form.parking}
+              onChange={handleFormChange}
+            />
             <label htmlFor="parking">Parking</label>
           </div>
 
           <div className="flex gap-2">
-            <input type="checkbox" id="furnished" />
+            <input
+              type="checkbox"
+              id="furnished"
+              checked={form.furnished}
+              onChange={handleFormChange}
+            />
             <label htmlFor="furnished">Furnished</label>
           </div>
         </div>
@@ -174,9 +245,11 @@ export default function AddHome() {
             <input
               type="number"
               id="numOfBedrooms"
+              value={form.numOfBedrooms}
               min="1"
               className="lg:w-1/3 w-1/2 outline outline-gray-500 focus:outline-blue-500 focus:outline-2 p-3 rounded-inputRadius"
               required
+              onChange={handleFormChange}
             />
             <label htmlFor="numOfBedrooms">Beds</label>
           </div>
@@ -185,9 +258,11 @@ export default function AddHome() {
             <input
               type="number"
               id="numOfBathrooms"
+              value={form.numOfBathrooms}
               min="1"
               className="lg:w-1/3 w-1/2 outline outline-gray-500 focus:outline-blue-500 focus:outline-2 p-3 rounded-inputRadius"
               required
+              onChange={handleFormChange}
             />
             <label htmlFor="numOfBathrooms">Baths</label>
           </div>
@@ -196,16 +271,20 @@ export default function AddHome() {
             <input
               type="number"
               id="price"
+              value={form.price}
               min="1"
-              className="lg:w-1/3 w-1/2 outline outline-gray-500 focus:outline-blue-500 focus:outline-2 p-3 rounded-inputRadius"
+              className="lg:w-2/3 w-full outline outline-gray-500 focus:outline-blue-500 focus:outline-2 p-3 rounded-inputRadius"
               required
+              onChange={handleFormChange}
             />
             <div className="flex flex-col">
               <label htmlFor="price">Price</label>
-              <span className="md:text-sm text-xs text-gray-500">
-                {" "}
-                $ / month
-              </span>
+              {form.type === "rent" && (
+                <span className="md:text-sm text-xs text-gray-500">
+                  {" "}
+                  $ / month
+                </span>
+              )}
             </div>
           </div>
         </div>
@@ -213,7 +292,7 @@ export default function AddHome() {
         {/* 2f. The image upload session  */}
         <div className="md:w-2/3 w-full flex flex-col gap-2 items-center m-2">
           {/* Heading */}
-          <h1 className="font-semibold">Upload images</h1>
+          <h1 className="font-semibold">Upload image</h1>
 
           {/* The images upload section and images are display in this container */}
           <div className="w-full max-h-96 flex flex-col gap-2 justify-between items-center border border-gray-500 p-3 rounded-lg overflow-scroll">
@@ -226,12 +305,19 @@ export default function AddHome() {
                 onChange={(e) => setImgFiles(e.target.files)}
                 multiple
               />
-              <CustomButton text={isUploading ? "Uploading.." : "Upload"} buttonHandler={handleImagesUpload} />
+              <CustomButton
+                text={isUploading ? "Uploading.." : "Upload"}
+                buttonHandler={handleImagesUpload}
+                disabledCondition={isUploading}
+              />
             </div>
             {form.imageUrls.length === 0 ? (
               <div className="flex flex-col gap-2 my-10 justify-center items-center">
                 <img src={emptyList} className="w-24" />
-                <span className="text-gray-400">Add images here</span>
+                <p className="text-gray-400 text-center">
+                  Add images here
+                  <span className="text-xs block">(Minimum one image required)</span>
+                </p>
               </div>
             ) : (
               form.imageUrls.map((url, index) => {
