439364a4cafe3cb7ebd26018035c361445f056ca
yanghd <yanghd@vt.edu>
2023-12-21 03:05:51 -0500

    style: Update some prop setting

--- frontend/src/components/utils/OverlayModal.jsx
+++ frontend/src/components/utils/OverlayModal.jsx
@@ -1,8 +1,9 @@
 import { motion } from "framer-motion";
 import { IoCloseCircleOutline } from "react-icons/io5";
+import Overlay from "./Overlay";
 export default function OverlayModal({
   isModalOpen,
-  img,
+  img=null,
   onCloseModal,
   modalButtonHandler,
   headerText,
@@ -10,19 +11,19 @@ export default function OverlayModal({
 }) {
   if (!isModalOpen) return null;
   return (
-    <div className="fixed bg-overlay h-full w-full top-0 left-0 z-20">
+    <Overlay>
       <motion.div
         initial={{ opacity: 0, translateY: "-100%", translateX: "-50%" }}
         animate={{ opacity: 1, translateY: "-50%", translateX: "-50%" }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.5 }}
-        className="flex flex-col gap-2 items-center fixed px-3 py-5 w-[40%] top-1/2 left-1/2 bg-white shadow-card rounded-lg z-30"
-      >
+        className="flex flex-col gap-2 items-center fixed px-3 py-5 md:w-[40%] w-[80%] top-1/2 left-1/2 bg-white shadow-card rounded-lg z-50"
+      >                 
         <IoCloseCircleOutline
           className="text-2xl self-end cursor-pointer"
           onClick={onCloseModal}
         />
-        <img src={img} className="object-cover w-1/2" />
+        {img &&  <img src={img} className="object-cover w-1/2" />}
         <h1 className="text-2xl text-center font-semibold">{headerText}</h1>
         {messageText && <p>{messageText}</p>}
         <div className="flex justify-between w-10/12 box-border">
@@ -44,6 +45,6 @@ export default function OverlayModal({
           </motion.button>
         </div>
       </motion.div>
-    </div>
+    </Overlay>
   );
 }
