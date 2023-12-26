fdf91a0e615f28122608f0ce89dd127c437577fc
yanghd <yanghd@vt.edu>
2023-12-26 15:38:32 -0500

    feat: Added an EmptyList UI component

---
+++ frontend/src/components/utils/EmptyList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function EmptyList({ image, text, subtext = null }) {
+  return (
+    <div className="flex flex-col justify-center items-center my-48 gap-4">
+      <img src={image} className="h-40 w-40" />
+      <span className="text-2xl text-gray-400">
+        {text}
+        {subtext && (
+          <span className="block text-center text-base text-blue-400">
+            {subtext}
+          </span>
+        )}
+      </span>
+    </div>
+  );
+}
