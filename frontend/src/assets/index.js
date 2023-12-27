0e8bed5e5819e0a75863c12219b0c8b9cc1ea872
yanghd <yanghd@vt.edu>
2023-12-26 20:37:22 -0500

    chore: Remove some unused assets

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,8 +1,6 @@
 import logo from "./logo2.svg";
 import blackLogo from "./logo2.svg";
-import registerHouse from "./register_house.jpeg";
-import registerMansion from "./register_mansion.jpg";
-import registerMansion2 from "./register_mansion2.jpg";
+import registerHouse from "./register_house2.jpg";
 import loginHouse from "./login_house.jpg";
 import loginMansion from "./login_mansion.jpg";
 import trashCan from "./trashcan.png";
@@ -14,8 +12,6 @@ export {
   logo,
   blackLogo,
   registerHouse,
-  registerMansion,
-  registerMansion2,
   loginHouse,
   loginMansion,
   trashCan,
