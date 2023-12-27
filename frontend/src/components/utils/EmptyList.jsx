a8317151065d93cdd7341168e8bb64d80138573f
yanghd <yanghd@vt.edu>
2023-12-26 20:38:09 -0500

    style: Change the width of the image

--- frontend/src/components/utils/EmptyList.jsx
+++ frontend/src/components/utils/EmptyList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function EmptyList({ image, text, subtext = null }) {
   return (
     <div className="flex flex-col justify-center items-center my-48 gap-4">
-      <img src={image} className="h-40 w-40" />
+      <img src={image} className="h-40 w-48" />
       <span className="text-2xl text-gray-400">
         {text}
         {subtext && (
