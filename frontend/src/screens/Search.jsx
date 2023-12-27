f79d35707d43203b4e83080692ad77fa71df6e65
yanghd <yanghd@vt.edu>
2023-12-26 19:37:49 -0500

    Style: Add some padding

--- frontend/src/screens/Search.jsx
+++ frontend/src/screens/Search.jsx
@@ -29,7 +29,7 @@ export default function Search() {
   }, [urlParams]);
 
   return (
-    <div className="pt-44">
+    <div className="pt-48">
       {isLoading ? (
         // 1. When loading, display a three dots animation in the middle of screen.
         <Loading />
