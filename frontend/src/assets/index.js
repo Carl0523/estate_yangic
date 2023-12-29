b641a49c24853545d452714b1944f835d12e0fa5
yanghd <yanghd@vt.edu>
2023-12-28 23:03:42 -0500

    chore: Add avatar and new logo

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,17 +1,16 @@
-import logo from "./logo2.svg";
-import blackLogo from "./logo2.svg";
+import logo from "./logo3.svg";
 import registerHouse from "./register_house2.jpg";
 import loginHouse from "./login_house.jpg";
 import loginMansion from "./login_mansion.jpg";
 import trashCan from "./trashcan.png";
 import emptyList from "./empty_list.png";
 import emptyHomeList from "./empty_home_list.png";
-import noResultFound from "./no_result_found.png"
-import landing from "./landing2.png"
+import noResultFound from "./no_result_found.png";
+import landing from "./landing2.png";
+import avatar from "./avatar.jpeg";
 
 export {
   logo,
-  blackLogo,
   registerHouse,
   loginHouse,
   loginMansion,
@@ -19,5 +18,6 @@ export {
   emptyList,
   emptyHomeList,
   noResultFound,
-  landing
+  landing,
+  avatar
 };
