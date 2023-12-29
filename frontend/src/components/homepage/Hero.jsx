31868013ff8e72d6adfaadc2d93fda4a5dff5107
yanghd <yanghd@vt.edu>
2023-12-28 23:18:33 -0500

    refactor: Change animation behavior

--- frontend/src/components/homepage/Hero.jsx
+++ frontend/src/components/homepage/Hero.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { landing } from "../../assets";
 import CustomButton from "../utils/CustomButton";
+
+import { motion } from "framer-motion";
+
 export default function Hero() {
   const navigate = useNavigate();
 
@@ -15,14 +18,24 @@ export default function Hero() {
     <div className="mt-24 mb-10 mx-5">
       <div className="sm:h-[500px] h-[600px] bg-purple-300 relative rounded-lg">
         {/* 1. The cover image */}
-        <img
+        <motion.img
+          initial={{ translateY: "-100%" }}
+          animate={{ translateY: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, type: "spring" }}
           src={landing}
           alt="Cover House"
           draggable="false"
           className="md:w-1/2 w-full md:h-full h-1/2 absolute right-0 bottom-0"
         />
         {/* 2. The introduction text, subtext, and explore button */}
-        <div className="flex flex-col gap-5 absolute md:top-[20%] top-[10%] xs:left-12 left-8 text-white">
+        <motion.div
+          initial={{ translateX: "-100%" }}
+          animate={{ translateX: 0 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, type: "spring" }}
+          className="flex flex-col gap-5 absolute md:top-[20%] top-[10%] xs:left-12 left-8 text-white"
+        >
           {/* 2A. The header text */}
           <h1 className="md:text-5xl xs:text-4xl text-3xl font-bold">
             Best path to find you
@@ -41,7 +54,7 @@ export default function Hero() {
             responsiveWidth="w-1/2"
             buttonHandler={handleExplore}
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
