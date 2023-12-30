47bbdb0947f661a4b17bfc21668197f335475046
yanghd <yanghd@vt.edu>
2023-12-30 02:13:50 -0500

    ops: allow cross origin access

--- backend/index.js
+++ backend/index.js
@@ -25,12 +25,19 @@ const port = process.env.PORT;
 
 const app = express();
 
+
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(cookieParser());
 
