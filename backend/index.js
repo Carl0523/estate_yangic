273abacd1c3df5671799d96f55e3a919aa98a425
yanghd <yanghd@vt.edu>
2023-12-30 02:17:54 -0500

    ops: update the origin

--- backend/index.js
+++ backend/index.js
@@ -28,16 +28,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://home-yonder.onrender.com",
     credentials: true,
   })
 );
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(cookieParser());
 
