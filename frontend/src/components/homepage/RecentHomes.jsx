035dc0fb0bc9bf68f65708b30e967d1894ec024a
yanghd <yanghd@vt.edu>
2023-12-29 22:33:51 -0500

    style: change the padding

--- frontend/src/components/homepage/RecentHomes.jsx
+++ frontend/src/components/homepage/RecentHomes.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
 import HomeCard from "../homes_list/HomeCard";
 
 export default function RecentHomes() {
@@ -24,30 +23,29 @@ export default function RecentHomes() {
       <h1 className="text-4xl font-bold">Recent Offers</h1>
 
       {/* 2. The list of recent offer */}
-      <div className="flex gap-5 overflow-x-auto flex-nowrap scroll-smooth p-3 border">
-          {recentHomes.map((home, index) => {
-            const formattedDate = new Date(home.createdAt).toLocaleDateString(
-              "en-US"
-            );
-            return (
-              <HomeCard
-                key={index}
-                price={home.price}
-                homeId={home._id}
-                address={home.address}
-                coverImage={home.imageUrls[0]}
-                date={formattedDate}
-                furnished={home.furnished}
-                numOfBathrooms={home.numOfBathrooms}
-                numOfBedrooms={home.numOfBathrooms}
-                parking={home.parking}
-                type={home.type}
-                showExtraInfo={false}
-                
-              />
-            );
-          })}
-        </div>
+      <div className="flex gap-5 overflow-x-auto flex-nowrap scroll-smooth p-5 border rounded-md">
+        {recentHomes.map((home, index) => {
+          const formattedDate = new Date(home.createdAt).toLocaleDateString(
+            "en-US"
+          );
+          return (
+            <HomeCard
+              key={index}
+              price={home.price}
+              homeId={home._id}
+              address={home.address}
+              coverImage={home.imageUrls[0]}
+              date={formattedDate}
+              furnished={home.furnished}
+              numOfBathrooms={home.numOfBathrooms}
+              numOfBedrooms={home.numOfBathrooms}
+              parking={home.parking}
+              type={home.type}
+              showExtraInfo={false}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
